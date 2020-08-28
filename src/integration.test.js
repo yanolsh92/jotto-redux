c957dc2ddcacfb0fb1fe965e801cfd82d1478388
yanolsh92 <yanolsh@gmail.com>
2020-08-28 23:30:31 +0300

    Set up gessWords test

---
+++ src/integration.test.js
@@ -0,0 +1,14 @@
+import { storeFactory } from '../test/testUtils';
+import { guessedWord } from './actions';
+
+describe('gessWord action dispatcher', () => {
+  describe('no guessed words', () => {
+    test('updates state correctly for unsuccessful guess', () => {});
+    test('updates state correctly for successful guess', () => {});
+  });
+
+  describe('some guessed words', () => {
+    test('updates state correctly for unsuccessful guess', () => {});
+    test('updates state correctly for successful guess', () => {});
+  });
+});
---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,11 @@
+import { actionTypes } from '../actions';
+
+/**
+ * @function guessedWordsReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @returns {boolean} - new guessedWord state
+ */
+export default (state, action) => {
+  return null;
+};
