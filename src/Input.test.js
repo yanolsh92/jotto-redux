d4afd6f6da2a016559fc2baf83f207af4df25b45
yanolsh92 <yanolsh@gmail.com>
2020-08-28 22:52:48 +0300

    Input component render component

--- src/Input.test.js
+++ src/Input.test.js
@@ -4,11 +4,6 @@ import { shallow } from 'enzyme';
 import { findByTestAttr, storeFactory } from '../test/testUtils';
 import Input from './Input';
 
-/**
- * Factory functio to create a ShallowWrapper for GuessedWords component
- * @param  {object} initialState={} - Initial state for this setup
- * @returns {ShallowWrapper}
- */
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
   const wrapper = shallow(<Input store={store} />)
@@ -16,17 +11,46 @@ const setup = (initialState = {}) => {
     .dive();
   return wrapper;
 };
+setup();
 
 describe('render', () => {
   describe('word has not been guessed', () => {
-    test('renders component without error', () => {});
-    test('renders input box', () => {});
-    test('renders submit button', () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
+    });
+    test('renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(1);
+    });
+    test('renders submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.length).toBe(1);
+    });
   });
   describe('word has been guessed', () => {
-    test('renders component without error', () => {});
-    test('does not render input box', () => {});
-    test('does not render submit button', () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
+    });
+    test('does not renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(0);
+    });
+    test('does not renders submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.length).toBe(0);
+    });
   });
 });
 describe('update state', () => {});
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,7 +1,6 @@
 import checkPropTypes from 'check-prop-types';
 import { createStore } from 'redux';
 import rootReducer from '../src/reducers';
-
 /** Create a testing store with imported reducers, middleware, and initial state.
  * gloabals: rootReducer.
  * @param  {objetc} initialState -Initial stat for store
@@ -9,7 +8,7 @@ import rootReducer from '../src/reducers';
  * @returns {Store} -Redux sotre
  */
 export const storeFactory = (initialState) => {
-  createStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState);
 };
 
 /**
@@ -22,11 +21,6 @@ export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
 };
 
-/**
- * Throw error if conformingProps do not pass propTypes validation.
- * @param {React.Component} component - Component to check props against.
- * @param {object} conformingProps - Props we expect to conform to defined propTypes.
- */
 export const checkProps = (component, conformingProps) => {
   const propError = checkPropTypes(
     component.propTypes,
